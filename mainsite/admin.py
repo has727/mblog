23f43d5084d96333e49e885a5c11fc3799daaca9
has727 <has727@outlook.com>
2017-08-25 23:27:38 +0800

    list display several info in admin site

--- mainsite/admin.py
+++ mainsite/admin.py
@@ -3,4 +3,11 @@ from mainsite.models import Post
 
 # Register your models here.
 
-admin.site.register(Post)
+
+class PostAdmin(admin.ModelAdmin):
+	list_display = ('title', 'slug', 'pub_date')
+	
+	
+	
+admin.site.register(Post, PostAdmin)
+
