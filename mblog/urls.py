772c912890b42439cdba7cae754a1f1fd90f0f40
has727 <has727@outlook.com>
2017-08-26 16:16:23 +0800

    1. minor changes, functionality remains the same

--- mblog/urls.py
+++ mblog/urls.py
@@ -15,14 +15,14 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
-# from mainsite.views import homepage, showpost
-from mainsite import views
+from mainsite.views import homepage, showpost
+# from mainsite import views
 
 
 urlpatterns = [
     # url(r'^', include('mainsite.urls', namespace='mainsite')),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', views.homepage),
-    url(r'^post/(.+)/$', views.showpost),
+    url(r'^$', homepage),
+    url(r'^post/(.+)/$', showpost),
    
 ]
